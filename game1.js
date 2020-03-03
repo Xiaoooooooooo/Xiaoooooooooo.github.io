18e491ea41ee8b6168fda62b50597cfdf0252911
Xiaoooooooooo <56744364+Xiaoooooooooo@users.noreply.github.com>
2020-03-02 14:42:24 -1000

    Add files via upload

--- game1.js
+++ game1.js
@@ -1,9 +1,10 @@
 function jumper() {
-  this.x = 30;
+  this.x = window.innerWidth/2;
   this.y = 0;
   this.gravity = 0.5; //the force of gravity
   this.lift = -12; // the opposing force of gravity when i jump
   this.velocity = 0;//speed of gravity force
+  this.speed = 10;
   
   //i am building a functipon to display on the screen. This is where i put my values of what the jumper will look like.
   this.show = function() {
@@ -24,8 +25,8 @@ function jumper() {
     this.y += this.velocity;
     this.velocity *= 0.9; //air resistance
     //this will prevent the jumper from leaving the ground
-    if (this.y > height) {
-      this.y = height;
+    if (this.y > height -25) {
+      this.y = height -25;
       this.velocity = 0;
     }
     //this will prevent the jumper from leaving the top of the screen
