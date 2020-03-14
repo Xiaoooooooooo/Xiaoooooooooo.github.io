c43133e464616dcde7c6434d521ab8b3f56e7ec8
Xiaoooooooooo <56744364+Xiaoooooooooo@users.noreply.github.com>
2020-03-14 00:09:11 -1000

    Add files via upload

--- bubble.js
+++ bubble.js
@@ -1,9 +1,9 @@
-function buff() {
+function bubble() {
   this.y = Math.floor(Math.random() * window.innerHeight);
   this.x = window.innerWidth - 25;
   this.speed = Math.floor((Math.random() * 6)+10); //speed of barrier
   this.velocity = 0;//speed of gravity force
-  this.time = 0;
+  this.time = Math.floor((Math.random() * 3));
   
   this.show = function() {
     fill(0,255,0);
@@ -13,12 +13,15 @@ function buff() {
   
   this.update = function() {
     this.x -= this.speed;
+    
     this.velocity = 0; //air resistance
-    if (this.x < 0 && this.time <= 0) {
+    if (this.x < 0) {
+      this.time = Math.floor((Math.random() * 3));
+      if (this.time > 1)
       this.x = window.innerWidth;
       this.y = Math.floor(Math.random() * window.innerHeight);
-      this.time = Math.floor((Math.random() * 6));
+      
       }
-    }
+    
   }
 }
