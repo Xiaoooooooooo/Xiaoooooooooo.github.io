cba61a26e8e9b66247f5eb44c7648f76a628f3c2
Xiaoooooooooo <56744364+Xiaoooooooooo@users.noreply.github.com>
2021-01-06 20:41:17 -1000

    Add files via upload

--- casino.js
+++ casino.js
@@ -2,17 +2,48 @@ function gid(el) {
   return document.getElementById(el);
 };
 
+function gcn(cn) {
+  return document.getElementsByClassName(cn);
+}
+
+gid("openPanel").onclick = function() {
+  gid("myPanel").style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == gid("myPanel")) {
+    gid("myPanel").style.display = "none";
+  }
+}
+
+function checking() {
+  if ((localStorage.currency) == "undefined") {
+      localStorage.currency = 1;
+  } 
+  else {
+    return
+  }
+}
+
+function adminPanel() {
+  gcn("adminPanel").classList.toggle("show")
+}
+
 gid("placeholder").innerHTML = "$" + localStorage.currency;
 
-function currency() {
+function currency(amount) {
   if(typeof(Storage) !== "undefined") {
-    if (localStorage.currency) {
-      localStorage.currency = Number(localStorage.currency)+1;
-    } else {
-      localStorage.currency = 1;
+    if (amount == 0) {
+      localStorage.currency = 0
+    }
+    else {
+      if (localStorage.currency) {
+        localStorage.currency = Number(localStorage.currency)+amount;
+      }
     }
     gid("placeholder").innerHTML = "$" + localStorage.currency;
-  } else {
+  } 
+  else {
     gid("error").innerHTML = "Error";
   }
 }
