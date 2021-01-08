17ee9240e26f0a821f6c6bf60acf2b597569972a
Xiaoooooooooo <56744364+Xiaoooooooooo@users.noreply.github.com>
2021-01-08 03:17:09 -1000

    object function and starting leaderboard

--- casino.js
+++ casino.js
@@ -16,52 +16,98 @@ window.onclick = function(event) {
     for (var i = 0; i < gcn("admin").length; i++) {
       gcn("admin")[i].style.display = "none";
     }
-    gid("panelPassword").style.display = "inline"
+    gid("panelPassword").style.display = "inline";
     gid("panelPassword").value = "";
   }
 }
 
-function panelPassword() {
-  if (gid("panelPassword").value == localStorage.admin) {
-    for (var i = 0; i < gcn("admin").length; i++) {
-      gcn("admin")[i].style.display = "inline";
-    }
-    gid("panelPassword").style.display = "none"
-  }
-  else {
-    gid("panelPassword").value = "";
-    localStorage.currency = Number(localStorage.currency)-1
-  }
-}
+var adminPanel = {
 
-function checking() {
-  if ((localStorage.currency) == undefined) {
-      localStorage.currency = 0;
-  } 
-  else {
-    return
-  }
-}
-
-function adminPanel() {
-  gcn("adminPanel").classList.toggle("show")
-}
-
-gid("placeholder").innerHTML = "$" + localStorage.currency;
-
-function currency(amount) {
-  if(typeof(Storage) !== "undefined") {
-    if (amount == 0) {
-      localStorage.currency = 0
+  panelPassword: function() {
+    if (gid("panelPassword").value == localStorage.admin) {
+      for (var i = 0; i < gcn("admin").length; i++) {
+        gcn("admin")[i].style.display = "inline";
+      }
+      gid("panelPassword").style.display = "none";
     }
     else {
-      if (localStorage.currency) {
-        localStorage.currency = Number(localStorage.currency)+amount;
-      }
+      gid("panelPassword").value = "";
+      localStorage.currency = Number(localStorage.currency)-1;
+      balance.minimum()
     }
+  },
+
+  currency: function(amount) {
+    if(typeof(Storage) !== "undefined") {
+      if (amount == 0) {
+        localStorage.currency = 0;
+      }
+      else {
+        if (localStorage.currency) {
+          localStorage.currency = Number(localStorage.currency)+amount;
+        }
+      }
+      balance.update()
+    } 
+    else {
+      gid("error").innerHTML = "Error";
+    }
+  },
+}
+
+var balance = {
+  checking: function() {
+    if ((localStorage.currency) == undefined) {
+      localStorage.currency = 0;
+      localStorage.name = "Anonymous";
+    } 
+    else {
+      return
+    }
+  },
+  update: function() {
     gid("placeholder").innerHTML = "$" + localStorage.currency;
-  } 
-  else {
-    gid("error").innerHTML = "Error";
+  },
+  minimum: function() {
+    if (localStorage.currency <= 0) {
+      localStorage.currency = 0;
+      balance.update();
+    }
+    else {
+      return
+    }
   }
+}
+
+balance.update()
+
+var localScoreBoard = {
+  data: [],
+
+  addScore: function(name, score) {
+    // Add the new score to end of 'data'
+    this.data.push([name, score]);
+
+    // Sort data by all of the scores
+    this.data.sort(function(a, b) {
+      return a[1] > b[1];
+    });
+
+    // Take just the top 5 elements
+    if (data.length > 5) {
+      this.data = this.data.splice(0, 5);
+    }
+  },
+
+  getScores: function() {
+    return this.data;
+  },
+
+  saveScores: function() {
+    localStorage.setItem('scoreboard', this.data);
+  },
+
+  loadScores: function() {
+    localStorage.getItem('scoreboard');
+  },
 }
